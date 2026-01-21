c0642d27afb194d0f5f522bff23ad088635868d8
Jithmal Habarakada <146130737+PanchaXD@users.noreply.github.com>
2026-01-21 11:24:14 +0100

    Positive, Negative, or Zero

---
+++ sign.py
@@ -0,0 +1,18 @@
+"""
+2. Positive, Negative, or Zero
+
+    Ask for one integer and print:
+        "Positive" if > 0
+        "Negative" if < 0
+        "Zero" if it is 0
+"""
+
+x = int(input("What's x? "))
+
+if x > 0:
+    print("Positive")
+elif x < 0:
+    print("Negative")
+else:
+    print("Zero")
+
