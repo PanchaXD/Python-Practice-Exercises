f3207142e6185d8f674f4b51c84f7b2cabb16de8
Jithmal Habarakada <146130737+PanchaXD@users.noreply.github.com>
2026-01-21 11:27:45 +0100

    Even or Odd (No Function)

---
+++ parity.py
@@ -0,0 +1,11 @@
+"""
+3. Even or Odd (No Function)
+    Ask for a number and print "Even" or "Odd" using %.
+"""
+
+x = int(input("What's x? "))
+
+if x % 2 == 0:
+    print("Even")
+else:
+    print("odd")
